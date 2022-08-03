48b01f78fb75506e64d47ab460d33c16cdb7b48f
Bipul Chaudhary <bipulchau@gmail.com>
2022-08-03 13:55:19 -0400

    create and run  the server

--- src/main/java/com/revature/main/Driver.java
+++ src/main/java/com/revature/main/Driver.java
@@ -3,6 +3,7 @@ package com.revature.main;
 import com.revature.dao.UserDao;
 import com.revature.model.User;
 import com.revature.utilty.ConnectionUtility;
+import io.javalin.Javalin;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,12 +11,9 @@ import java.util.List;
 
 public class Driver {
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-        try {
-            System.out.println(userDao.deleteUserByUserName("donaldson21"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Javalin app = Javalin.create();
+                app.start(8080);
+        app.get("/", ctx -> ctx.result("Hello World"));
 
     }
 }
