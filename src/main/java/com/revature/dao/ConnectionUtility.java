8ac718c4a3de7fe7b9dd316f2d63ef9edda73004
Bipul Chaudhary <bipulchau@gmail.com>
2022-08-02 21:04:59 -0400

    Connection with database success

--- src/main/java/com/revature/dao/ConnectionUtility.java
+++ src/main/java/com/revature/dao/ConnectionUtility.java
@@ -1,4 +1,19 @@
 package com.revature.dao;
 
+
+
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionUtility {
+    public static Connection createConnection() throws SQLException {
+        Driver postgresDriver = new Driver();
+        DriverManager.registerDriver(postgresDriver);
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres");
+        return con;
+    }
 }
--- src/main/java/com/revature/main/Driver.java
+++ src/main/java/com/revature/main/Driver.java
@@ -1,7 +1,18 @@
 package com.revature.main;
 
+import com.revature.dao.ConnectionUtility;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Driver {
     public static void main(String[] args) {
-
+        try {
+            Connection con = ConnectionUtility.createConnection();
+            System.out.println(con);
+        } catch (SQLException e) {
+            System.out.println("Not done");
+            throw new RuntimeException(e);
+        }
     }
 }
